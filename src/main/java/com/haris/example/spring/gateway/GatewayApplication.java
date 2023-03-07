231a254a50a60494246f2ced30681a1f5e583f47
mukhtar haris <mukhtarharis.lethalaric@gmail.com>
2023-03-07 20:06:29 +0700

    initial commit

---
+++ src/main/java/com/haris/example/spring/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.haris.example.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haris/example/spring/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haris.example.spring.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
